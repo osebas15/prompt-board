dfa2af7cefacd04127e18d93e6a13cbd8e7bb250
Sebastian Aguirre <osebas15@gmail.com>
2025-07-12 18:04:29 -0400

    all test pass

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -120,11 +120,11 @@ export const Modal = ({
     <div
       className="fixed inset-0 z-50 flex items-center justify-center p-4"
       data-testid="modal-overlay"
+      onClick={handleOverlayClick}
     >
       {/* Backdrop */}
       <div
         className="absolute inset-0 bg-black bg-opacity-50 transition-opacity"
-        onClick={handleOverlayClick}
         aria-hidden="true"
       />
       
